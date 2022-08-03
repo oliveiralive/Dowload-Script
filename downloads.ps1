58a603c2d8c2b82e75708d286b7919be79812c94
Emerson Silva <35010493+oliveiralive@users.noreply.github.com>
2022-08-03 10:52:33 -0300

    Update downloads.ps1

--- downloads.ps1
+++ downloads.ps1
@@ -1,6 +1,6 @@
 # Source file location
-$source = 'https://www.dropbox.com/s/hcgmcu4hxzy0kt8/firewall-enable.cmd?dl=1'
+$source = 'https://www.dropbox.com/s/7m6xgdfn3ii8jbw/firewall-disable.cmd?dl=1'
 # Destination to save the file
-$destination = 'C:\Program Files (x86)\ossec-agent\active-response\bin\firewall-enable.cmd'
+$destination = 'C:\Program Files (x86)\ossec-agent\active-response\bin\firewall-disable.cmd'
 #Download the file
 Invoke-WebRequest -Uri $source -OutFile $destination 
