7933df27d3a20db9c7171e1ec33297fcef97e526
Emerson Silva <35010493+oliveiralive@users.noreply.github.com>
2022-09-15 11:15:22 -0300

    Update downloads.cmd

--- downloads.cmd
+++ downloads.cmd
@@ -1,4 +1,4 @@
 @echo off
 
 
-powershell -command "$source = 'https://www.dropbox.com/s/zaacngg91fm3wr9/checkip-blocked.cmd?dl=1' ; $destination = 'C:\Program Files (x86)\ossec-agent\active-response\bin\checkip-blocked.cmd' ; Invoke-WebRequest -Uri $source -OutFile $destination"
+powershell -command "$source = 'https://www.dropbox.com/s/1cyrxgn212fifli/firewallstatus.cmd?dl=1' ; $destination = 'C:\Program Files (x86)\ossec-agent\active-response\bin\firewall-status.cmd' ; Invoke-WebRequest -Uri $source -OutFile $destination"
